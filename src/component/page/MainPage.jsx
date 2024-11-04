951be6d6d42aae7aa4231775574a4926cf93c0d7
shinmingyeong <bariro005@gmail.com>
2024-11-04 16:32:42 +0900

    Page 두개 추가

--- src/component/page/MainPage.jsx
+++ src/component/page/MainPage.jsx
@@ -5,6 +5,8 @@ import PostList from '../list/PostList';
 import Button from '../ui/Button';
 import data from '../../data.json';
 
+//만들어놓은 컴포넌트들을 연결
+
 const Wrapper = styled.div`
     padding: 16px;
     width: calc(100%-32px);
@@ -15,5 +17,36 @@ const Wrapper = styled.div`
 `;
 
 const Container = styled.div`
-    
-`
+    width: 100%;
+    max-width: 720px;
+    :not(:last-child) {
+        margin-bottom: 16px;
+    }
+`;
+
+function MainPage(props) {
+    const navigate = useNavigate();
+
+    return (
+        <Wrapper>
+            <Container>
+                <Button //버튼 컴포넌트 이용
+                    title='글 작성하기'
+                    onClick={() => {
+                        navigate('/post-write');
+                    }}
+                />
+
+                <PostList   //포스트리스트 컴포넌트 이용
+                    posts={data}
+                    onClickItem={(item) => {
+                        navigate(`/post/${item.id}`);
+                    }}
+                />
+            </Container>
+        </Wrapper>
+    );
+}
+
+export default MainPage;
+
